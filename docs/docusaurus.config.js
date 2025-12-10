81bce9fcd6876d8f4f67a15ed36f76b77b887e6d
maamirkh <maamirkhan1983@gmail.com>
2025-12-11 00:06:33 +0500

    updated

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -20,10 +20,10 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-organization.github.io',
+  url: 'http://localhost:3000',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/Humanoid-Robotics-Textbook/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
